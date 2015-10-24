921c718a933f14aad67dfa0069726c97250ca7b0
Matt Hernandez <dahv24@gmail.com>
2015-10-24 09:59:04 -0500

    initial commit

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Provisioning virtual machine..."
+# ---------------------------------------
+#          Virtual Machine Setup
+# ---------------------------------------
+
+echo "Adding multiverse sources."
+cat > /etc/apt/sources.list.d/multiverse.list << EOF
+deb http://archive.ubuntu.com/ubuntu trusty multiverse
+deb http://archive.ubuntu.com/ubuntu trusty-updates multiverse
+deb http://security.ubuntu.com/ubuntu trusty-security multiverse
+EOF
+
+echo "Updating packages"
+apt-get update
+
+echo "Installing GIT"
+apt-get install git -y > /dev/null
